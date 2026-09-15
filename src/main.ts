fd1fac180e031f0c07f112d51474a14f7a33ebd5
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-09-15 15:51:58 +0500

    Load sandbox library styles

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import './ui-ux-pass-01.css';
 import './feel-art-audio-pass-01.css';
 import './ui-ux-overhaul-02.css';
 import './sandbox-core.css';
+import './sandbox-library.css';
 import { bootstrapSquishyApp } from './app/bootstrap';
 import { getGameCopy, normalizeLanguage } from './i18n';
 
